c8faf422622e3affa064f1496d1958c51146ea81
Battarasetty <Harish93601@gmail.com>
2023-12-26 08:12:57 +0530

    Login Page UI added

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,8 @@ import './App.css'
 import Home from './Pages/Home'
 import ProductList from './Pages/ProductList'
 import Product from './Pages/Product'
+import Register from './Pages/Register'
+import Login from './Pages/Login'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,7 +16,9 @@ function App() {
       <div>
         {/* <Home /> */}
         {/* <ProductList /> */}
-        <Product />
+        {/* <Product /> */}
+        {/* <Register /> */}
+        <Login />
       </div>
     </>
   )
